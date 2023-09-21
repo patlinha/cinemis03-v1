290478bebe4a478dd180b67cbfddc64ca95131f6
patricia <areiasteste@gmail.com>
2023-09-20 22:56:18 -0300

    correcaoFilmes_02

--- main.js
+++ main.js
@@ -83,19 +83,168 @@ const catalogo = [
         sinopse: "Durante a violenta guerra entre Irã e Iraque, alguns professores curdos perambulam pelas colinas das fronteiras do Irã com quadros negros às suas costas, em busca de alunos e de aprendizes. Said e Reeboir acabam unidos nesta tarefa, liderando um grupo de crianças as quais precisam transmitir seus conhecimentos e manter vivas. Composto por atores amadores, o elenco conta apenas com um intérprete profissional, Bahman Ghobadi, que vive o professor Reeboir. Os restantes dos participantes eram residentes locais e foram recrutados pela equipe de filmagem por lá mesmo. A cineasta iraniana Samira Makhmalbaf - filha do realizador Mohsen Makhmalbaf ('Salam Cinema' e 'Gabbeh') – tinha apenas 18 anos quando estreou na direção e fez 'A Maçã'. Dois anos depois surge o seu segundo filme: 'O Quadro Negro'. Com ele, Samira foi a mais jovem presença em Cannes 2000, tendo sido galardoada com o Prémio do Júri.",
         imagem: "src=./imagens/filmes/quadroNegro.PNG",
       },
-      
+
+	{
+        id: 22,
+        titulo: "MEPHISTO",
+        direcao: "Istvan Szabo",
+        ciclo: "Cineclube Outubro",
+        curadoria: "Cineclube Outubro",
+        data: "20/09/2023",
+        horario: "19:30",
+        ano: "1981",
+        sinopse: "Alemanha, 1930. Hendrik Höfgen (Klaus Maria Brandauer), é um ambicioso ator que não se interessa por política, se dedicando somente à sua carreira. Porém, quando os nazistas começam a tomar o poder, ele aproveita a oportunidade para interpretar peças de propaganda nazista para o Reich, e logo acaba se transformando no mais popular ator da Alemanha.'Mefhisto' é baseado no romance homônimo de Klaus Mann (o livro ficou 40 anos proibido na Alemanha), escrito durante o exílio do escritor na Holanda, em 1936. 'Mefhisto', obra-prima do cinema, vencedor dos prêmios mais importante do cinema: Oscar de melhor filme estrangeiro, Palma de Ouro de melhor roteiro em Cannes e melhor filme escolhido pela crítica europeia em 1982.",
+        imagem: "src=./imagens/filmes/mephisto.png",
+      },
+      {
+        id: 23,
+        titulo: "Pink Floyd - The Wall",
+        direcao: "Alan Parker",
+        ciclo: "Sessão Ópera Rock",
+        curadoria: "Vitor Valim",
+        data: "21/09/2023",
+        horario: "19:30",
+        ano: "1982",
+        sinopse: "A história do 'The Wall' é contada apenas com a música do Pink Floyd, imagens e efeitos naturais. Não há diálogos convencionais para o progresso da narrativa. Nossa história é sobre Pink, um astro do rock and roll que se senta trancado em um quarto de hotel, em algum lugar em Los Angeles. Muitos shows, muitas drogas, muito aplauso: um caso perdido. Na TV, um filme de guerra demasiado familiar preenche a tela. Uma mistura de tempo e lugar, realidade e pesadelo quando entramos nas memórias dolorosas de Pink, cada uma um 'tijolo' no muro que ele tem gradualmente construído em torno de seus sentimentos. ",
+        imagem: "src=./imagens/filmes/wall.png",
+      },
+      {
+        id: 24,
+        titulo: "O ÚLTIMO FILME",
+        direcao: "Dennis Hopper",
+        ciclo: "História do cinema – Ciclo anos 1970: O cinema político pelo mundo e a ascensão dos blockbuster estadunidenses.",
+        curadoria: "Claudia Bortolato",
+        data: "22/09/2023",
+        horario: "15:00",
+        ano: "1971",
+        sinopse: "Este segundo filme dirigido pelo Dennis Hopper é caótico, indulgente e emocionante. Um dublê de cinema vai filmar um western nos Andes e se envolve com uma jovem prostituta de lá. Sua equipe retorna a Hollywood, mas ele continua no local, preso aos mitos de seus próprios filmes, nessa alegoria à destruição da inocência.",
+        imagem: "src=./imagens/filmes/ultimoFilme.png",
+      },
+	{
+        id: 25,
+        titulo: "POSSESSÃO",
+        direcao: "Andrzej Zulawski",
+        ciclo: "Grandes Diretores",
+        curadoria: "Ricardo Pereira",
+        data: "22/09/2023",
+        horario: "19:30",
+        ano: "1981",
+        sinopse: "Após retornar de uma longa viagem, tudo que Marc quer é encontrar sua esposa Anna e seu filho novamente. No entanto, assim que chega à Berlim, Marc percebe que Anna mudou radicalmente de comportamento e, assim que pode, ela pede o divórcio. O homem inicialmente suspeita que Anna foi infiel a ele, contudo, os eventos dão uma guinada sinistra e Marc percebe que está em uma situação macabra. Com Isabelle Adjani e Sam Neill.",
+        imagem: "src=./imagens/filmes/possessao.png",
+      },
+	{
+        id: 26,
+        titulo: "O ENÍGMA DE ANDRÔMEDA",
+        direcao: "Robert Wise",
+        ciclo: "Cine Voyager",
+        curadoria: "Carolina Idelfonço",
+        data: "23/09/2023",
+        horario: "16:00",
+        ano: "1971",
+        sinopse: "Um satélite espacial sai de órbita e cai em uma cidadezinha da Terra. Gerada pela colisão, uma bactéria fatal e misteriosa começa a dizimar a população. Uma equipe de cientistas trabalha em um laboratório subterrâneo para encontrar a cura e descobre que apenas uma criança e um bêbado sobreviveram. A ansiedade aumenta, ao mesmo tempo em que os pesquisadores correm para encontrar uma solução, antes que a humanidade seja exterminada.",
+        imagem: "src=./imagens/filmes/andromeda.png",
+      },
+
 	    {
-        id: 33,
+        id: 27,
         titulo: "SEXTA-FEIRA 13 PARTE II",
         direcao: "Steve Miner",
         ciclo: "Terror Slasher",
         curadoria: "Claudia Bortolato",
-        data: "30/09/2023",
+        data: "23/09/2023",
         horario: "19:30",
         ano: "1981",
         sinopse: "O segundo filme da série de terror se concentra em um grupo de adolescentes que se reúne no acampamento Crystal Lake para treinamento, sob a tutela do conselheiro chefe Paul. Inevitavelmente, Paul relata a história de Jason Voorheees, um menino que se afogou no campo e cuja mãe assassinou um grupo de conselheiros como vingança. Ninguém leva a história a sério até que Jason começa, de um modo terrível, a eliminar as pessoas",
         imagem: "src=./imagens/filmes/sexta13.png",
       },
+
+      {
+        id: 28,
+        titulo: "Um corpo que cai",
+        direcao: "Alfred Hitchcock",
+        ciclo: "Mestres do Suspense",
+        curadoria: "Cláudia Bortolato",
+        data: "24/09/2023",
+        horario: "16:00",
+        ano: "1958",
+        sinopse: "Em São Francisco, o detetive aposentado John 'Scottie' Ferguson (James Stewart) sofre de um terrível medo de alturas. Certo dia, encontra com um antigo conhecido, dos tempos de faculdade, que pede que ele siga sua esposa, Madeleine Elster (Kim Novak). John aceita a tarefa e fica encarregado da mulher, seguindo-a por toda a cidade. Ela demonstra uma estranha atração por lugares altos, levando o detetive a enfrentar seus piores medos. Ele começa a acreditar que a mulher é louca, com possíveis tendências suicidas, quando algo estranho acontece nesta missão. ",
+        imagem: "src=./imagens/filmes/corpoCai.png",
+      },
+	{
+        id: 29,
+        titulo: "PADRINHOS DE TOKYO",
+        direcao: "Satoshi Kon",
+        ciclo: "Curtindo Anime",
+        curadoria: "Andréa Reis",
+        data: "26/09/2023",
+        horario: "14:00",
+        ano: "2003",
+        sinopse: "A terceira produção de Satoshi Kon, um dos mais interessantes diretores de anime da atualidade. Hana uma ex drag queen, Gin um antigo ciclista alcoolista e Miyuki, uma adolescente fugitiva são três sem-teto que em sua tumultuada existência encontram um bebê abandonado no dia de natal. Os três resolvem então peregrinar em busca da solução do mistério e encontrar os pais da criança antes da chegada do ano novo. Durante a busca, deparam-se com fatos ocultos sobre o passado que são forçados à confrontar, ao mesmo tempo que aprendem a enfrentar o futuro juntos",
+        imagem: "src=./imagens/filmes/tokyo.png",
+      },
+	{
+        id: 30,
+        titulo: "DON'T THINK I'VE FORGOTTEN: CAMBODIA'S LOST ROCK & ROLL",
+        direcao: "John Pirozzi",
+        ciclo: "Cine Estrépita",
+        curadoria: "Carolina Idelfonço",
+        data: "27/09/2023",
+        horario: "19:30",
+        ano: "2014",
+        sinopse: "Durante as décadas de 1960 e 1970, o Camboja viveu anos de tranquilidade e certo agito cultural. Com forte influencia francesa (de quem foi colônia entre 1867 e 1953), os cambojanos entraram no mundo do Rock'n'Roll através dos europeus. Mais suave e comportado que o original americano, o Yê-Yê, como era chamado, ganhou sotaque oriental, sonoridades locais e personalidade própria. Essa cena crescente foi brutalmente interrompida pela irrupção do Khmer Vermelho durante a guerra do Vietnam. Mas deixou um legado de grandes talentos, que hoje é alvo de colecionadores no mundo inteiro.",
+        imagem: "src=./imagens/filmes/cambodia.png",
+      },
+	{
+        id: 31,
+        titulo: "A ESTRATÉGIA DA ARANHA",
+        direcao: "Bernardo Bertolucci",
+        ciclo: "História do cinema – Ciclo anos 1970: O cinema político pelo mundo e a ascensão dos blockbuster estadunidenses.",
+        curadoria: "Claudia Bortolato",
+        data: "29/09/2023",
+        horario: "15:00",
+        ano: "1971",
+        sinopse: "Produzido direto para a televisão, “A Estratégia da Aranha” é um drama/mistério político com roteiro baseado no conto “Tema do Traidor e do Herói” (1944), do autor argentino Jorge Luis Borges. Protagonizado por Giulio Brogi e Alida Valli, o filme segue o filho de um mártir antifascista que viaja para a cidade natal do pai e encontra o monumento a ele dedicado depredado. Isso faz com que ele se envolva na busca da verdade sobre sua vida e sobre suas ideias; no meio do caminho, acaba descobrindo segredos do seu pai, morto em circunstâncias estranhas quase 30 anos antes. À medida em que Athos desvenda uma teia de mentiras que parece envolver toda a cidade, mais ele se vê enredado nessa mesma teia. ",
+        imagem: "src=./imagens/filmes/aranha.png",
+      },
+
+      {
+        id: 32,
+        titulo: "CARRIE, A ESTRANHA",
+        direcao: "Brian de Palma",
+        ciclo: "Grandes Diretores",
+        curadoria: "Ricardo Pereira",
+        data: "29/09/2023",
+        horario: "19:30",
+        ano: "1976",
+        sinopse: "Nesta adaptação do romance de Stephen King, a quieta e sensível adolescente Carrie White enfrenta insultos dos colegas na escola e abuso em casa de sua mãe, uma fanática religiosa. Quando estranhos acontecimentos começam a acontecer em torno de Carrie, ela começa a suspeitar que tem poderes sobrenaturais. Com Sissy Spacek, John Travolta, Piper Laurie, Amy Irving, Nancy Allen e William Katt.",
+        imagem: "src=./imagens/filmes/carrie.png",
+      },
+
+      {
+        id: 33,
+        titulo: "RESPIRE FUNDO",
+        direcao: "Amy Koppelman",
+        ciclo: "Mulheres na Direção",
+        curadoria: "Nayara Lopes",
+        data: "30/09/2023",
+        horario: "16:00",
+        ano: "2021",
+        sinopse: "Julie é uma autora de livros infantis e agora mãe de primeira-viagem que foge para o brilhante mundo colorido de Crayola, criado por ela mesma, a fim de deixar para trás a escuridão causada por sua depressão pós-parto.",
+        imagem: "src=./imagens/filmes/respire.png",
+      },
+      {
+        id: 34,
+        titulo: "O Som ao Redor",
+        direcao: "Kleber Mendonça Filho",
+        ciclo: "Cineclube Estação Psicanálise",
+        curadoria: "Coletivo Estação Psicanálise",
+        data: "30/09/2023",
+        horario: "19:30",
+        ano: "2012",
+        sinopse: "A vida dos residentes de uma rua de classe média do Recife toma um rumo inesperado quando uma empresa de segurança particular é contratada para trazer paz aos moradores. Para alguns deles a presença dos guardas cria mais tensão do que alívio. Ao mesmo tempo, casada e mãe de duas crianças, Bia (Maeve Jinkings) precisa encontrar uma forma de lidar com os latidos estridentes do cachorro do vizinho.",
+        imagem: "src=./imagens/filmes/somRedor.png",
+      },
+
   ];
   
   for (const filmeCatalogo of catalogo) {
@@ -107,7 +256,7 @@ const catalogo = [
         <div class="card--titulo-data-hora">
           <div class="data-horario">
             <p>${filmeCatalogo.data}</p>
-            <p class="dia-semana">SÁB</p>
+            <!--<p class="dia-semana">SÁB</p>-->
             <p> ${filmeCatalogo.horario}</p>
           </div>  
           <p class="titulo-filme">${filmeCatalogo.titulo}</p>
@@ -117,7 +266,7 @@ const catalogo = [
           <p>Ciclo: ${filmeCatalogo.ciclo}</p>
           <p>Curadoria: ${filmeCatalogo.curadoria}</p>
           <p>Ano: ${filmeCatalogo.ano}</p>
-          <button id="btn-favoritar">Favoritar <i class="fa-regular fa-heart"></i></button>
+          <!--<button id="btn-favoritar">Favoritar <i class="fa-regular fa-heart"></i></button>-->
         </div>
       </div>
       `;
